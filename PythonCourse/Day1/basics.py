54d5f4dde4120d392508693a7a355e6c839faffb
Suraj Naik <suraj.b.naik@live.com>
2023-02-13 10:31:55 +0530

    updates

---
+++ PythonCourse/Day1/basics.py
@@ -0,0 +1,21 @@
+#My first python program
+print("Hello World!")
+
+#Using new line characters
+print("First Line string\nSecond line string")
+
+#String concatenation
+print("Hello" + "Suraj")
+
+#Input function
+print("Hello " + input("What's your name?") + "!")
+
+#Length of the string - 1
+print(len(input("What is your name? ")))
+
+#Length of the string - 2
+print(str(len(input("What is your name? "))) + ' is the length of your name')
+
+#Python variables
+name = input("What is your name ? ")
+print(name)
