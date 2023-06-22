154700d63f8aada4576d931c711f07d3c3401243
“Suraj <suraj.naik@bp.com>
2023-06-22 11:10:18 +0530

    api auth updates

--- PythonCourse/APIs/apiAuth.py
+++ PythonCourse/APIs/apiAuth.py
@@ -1,1 +1,18 @@
 # Use this site: https://openweathermap.org/ 
+
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+
+weather_params = {
+
+    "lat" : 14.661000,
+    "lon" : 74.304604,
+    "appid" : jdfkljsdklfjsdjflsd
+}
+
+response = requests.get(OWM_Endpoint,params=weather_params)
+response.raise_for_status()
+
+data = response.json()
+print(data)
