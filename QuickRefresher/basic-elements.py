c48d06ae6053550ba1c00962c569331c531fcb10
Suraj Naik <suraj.b.naik@live.com>
2025-07-13 08:59:48 +0530

    basic elements python

---
+++ QuickRefresher/basic-elements.py
@@ -0,0 +1,18 @@
+variable_name = "Learning python"
+print(variable_name)
+
+mathematical_operation = 3 + 9
+print(mathematical_operation)
+
+power_operator = 5 ** 6
+print(power_operator)
+
+array_example = [1, 4, 7, 18, 23]
+print('Differnet types of array printing:', array_example[:3], array_example[1:4], array_example[2:])
+
+print('Using parametes {input1} and {input2} in a string'.format(input1=variable_name, input2=mathematical_operation))
+
+#Using boolean operators
+boolean_response_true = 1 < 2
+boolean_response_false = 3 > 5
+print(boolean_response_true, boolean_response_false)
---
+++ QuickRefresher/control-flows.py
@@ -0,0 +1,9 @@
+#Using if, elif, and else statements in Python
+if 1 > 2:
+    print('yes')
+elif:
+    print('maybe')
+else:
+    print('no')
+
+
