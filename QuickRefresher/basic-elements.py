23e47d5bad104abfd7aaa052d9b597588b6b3e85
Suraj Naik <suraj.b.naik@live.com>
2025-07-13 12:01:31 +0530

    few more important python functions

--- QuickRefresher/basic-elements.py
+++ QuickRefresher/basic-elements.py
@@ -1,11 +1,23 @@
+#Using strings
 variable_name = "Learning python"
 print(variable_name)
+
+#Using string methods
+variable_name.split(' ')
+print(variable_name.upper())
 
 mathematical_operation = 3 + 9
 print(mathematical_operation)
 
+#Using power operator
 power_operator = 5 ** 6
 print(power_operator)
+
+#Data type conversion
+int_to_float = float(3)
+print(int_to_float)
+float_to_int = int(3.14)
+print(float_to_int)
 
 array_example = [1, 4, 7, 18, 23]
 print('Differnet types of array printing:', array_example[:3], array_example[1:4], array_example[2:])
@@ -24,14 +36,40 @@ dictionary_example = {
 }
 print(dictionary_example['what'])
 
+#Using dictionary methods
+dictionary_example['when'] = 'now'
+print('Dictionary after adding "when":', dictionary_example)
+dictionary_example.pop('how')  # Remove key 'how'
+dictionary_example.items()  # Get all items in the dictionary
+dictionary_example.keys()  # Get all keys in the dictionary
+dictionary_example.values()  # Get all values in the dictionary
+
 #Using Lists
 list_example = [1, 'item2', 2.5, True]
 print(list_example)
+
+#List operations
+list_example.append('new_item')
+print('List after appending:', list_example)
+list_example.pop(2)  # Remove item at index 2
+print('List after popping index 2:', list_example)
 
 #Using tuples
 tuple_example = (1, 'item2', [1,2,3], False)
 print(tuple_example)
 
+#Tuple unpacking
+tuple_unpacking_example = [(1, 2), (3, 4), (5, 6)]
+for a, b in tuple_unpacking_example:
+    print(f'Unpacked values: {a}, {b}')
+
 #Using sets
 set_example = {1, 1, 2, 2, 2, 2, 3, 4, 5}
 print(set_example)
+
+#using in operator
+1 in set_example
+print('1 is in set_example:', 1 in set_example)
+
+'x' in variable_name
+print("'x' is in variable_name:", 'x' in variable_name)
--- QuickRefresher/control-flows.py
+++ QuickRefresher/control-flows.py
@@ -1,9 +1,55 @@
 #Using if, elif, and else statements in Python
 if 1 > 2:
     print('yes')
-elif:
+elif 2 == 4:
     print('maybe')
 else:
     print('no')
 
 
+#for loops
+array_example = [1, 4, 7, 18, 23]
+for number in array_example:
+    print(number)
+
+#while loop
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
+
+#range function
+for i in range(0, 5, 1):
+    print(i)
+
+#List comprehensions
+list_comprehension = [x * 2 for x in range(10)]
+print('List comprehension:', list_comprehension)
+
+#functions is python
+#DOC stings in python within functions very important. Can be accessed using help(function_name)
+def square_number(x):
+    """Function to return the square of a number."""    
+    return x * x
+print('Square of 5:', square_number(5))
+help(square_number)
+
+#Using map function
+sequence = [1, 2, 3, 4, 5]
+mapped_sequence = map(square_number, sequence)
+mapped_sequence = list(mapped_sequence) #Convert map object to list
+print('Mapped sequence:', mapped_sequence)
+
+#Using lambda functions
+using_lambda = lambda input: input * 2
+print('Using lambda on 5:', using_lambda(5))
+
+#Using map and lambda together
+mapped_with_lambda = map(lambda x: x * 2, sequence)
+mapped_with_lambda = list(mapped_with_lambda)  # Convert map object to list
+print('Mapped with lambda:', mapped_with_lambda)
+
+#Using filter function
+fitered_sequence = filter(lambda num : num%2==0, sequence)
+fitered_sequence = list(fitered_sequence)  # Convert filter object to list
+print('Filtered sequence (even numbers):', fitered_sequence)
